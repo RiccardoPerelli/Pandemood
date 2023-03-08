c2e54bf3434d5ed05b7dcc61e173cfc27f4d4e33
RiccardoPerelli <39298954+RiccardoPerelli@users.noreply.github.com>
2023-03-08 12:30:45 +0100

    Initialize Andrea

---
+++ Pandemood Project/Assets/Scripts/General/InteractableObject.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+namespace General
+{
+    public class InteractableObject : MonoBehaviour
+    {
+        public bool pickedUp = false;
+        public bool cantPickUp = false;
+        public float distanceInteractionKeyImage = 2f;
+        public GameObject KeyInteractionImage;
+
+        private bool playerNear;
+        GameObject Player;
+
+        private void Start()
+        {
+            if(KeyInteractionImage!=null)
+                KeyInteractionImage.SetActive(false);
+
+            Player = GameObject.FindGameObjectWithTag("Player"); 
+        }
+
+        private void Update()
+        {
+            //VISIBLE KEY INTERACTION
+            if(!pickedUp && playerNear)
+            {
+                if(KeyInteractionImage!=null)
+                    KeyInteractionImage.SetActive(true);
+            }
+
+            //HIDE KEY INTERACTION
+            if (pickedUp || !playerNear)
+            {
+                if (KeyInteractionImage != null)
+                    KeyInteractionImage.SetActive(false);
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            //--------- CONSIDER THE CLOSEST OBJECT CAN PICK UP ----------
+            if (Player != null)
+                ControlDistanceFromPlayer();
+        }
+
+
+        //-----------SET CLOSEST OBJECT CAN PICK UP--------------
+        private void ControlDistanceFromPlayer()
+        {
+            var diff =Vector3.Distance(Player.transform.position,transform.position);
+            playerNear = diff < distanceInteractionKeyImage;
+        }
+    }
+}
