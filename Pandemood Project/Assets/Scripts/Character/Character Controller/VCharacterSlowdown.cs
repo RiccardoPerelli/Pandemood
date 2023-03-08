f568bf05c30b73ff71da1a0d5a4c8f2ae897aedc
RiccardoPerelli <39298954+RiccardoPerelli@users.noreply.github.com>
2023-03-08 12:26:52 +0100

    Initialize level_3_merge

---
+++ Pandemood Project/Assets/Scripts/Character/Character Controller/VCharacterSlowdown.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Slowdown;
+using UnityEngine;
+
+namespace Character.Character_Controller
+{
+    public class VCharacterSlowdown : MonoBehaviour
+    {
+        [SerializeField] private TimeScaling timeScaling;
+
+        // Update is called once per frame
+        private void Update()
+        {
+            if (Input.GetButtonDown("Slowdown"))
+            {
+                timeScaling.StartSlowdown();
+            }
+        }
+    }
+}
