991a99256fa5d6619aac3fc7d6c69a5773db2be3
RiccardoPerelli <39298954+RiccardoPerelli@users.noreply.github.com>
2023-03-08 12:34:12 +0100

    Initialize level4_fix

---
+++ Pandemood Project/Assets/Scripts/UI/characterUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class characterUI : MonoBehaviour
+{
+    public void setFemale()
+    {
+        PlayerPrefs.SetInt("Character", 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void setMale()
+    {
+        PlayerPrefs.SetInt("Character", 0);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ Pandemood Project/Assets/Scripts/UI/inGameMenu.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class inGameMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUi;
+    public string sceneToLoad;
+
+    void Start()
+    {
+        pauseMenuUi.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUi.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+    void Pause()
+    {
+        pauseMenuUi.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
