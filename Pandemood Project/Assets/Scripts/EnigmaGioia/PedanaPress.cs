bdfb3c8345a5d6e5625cc4f01e585bc1364cdbe8
RiccardoPerelli <39298954+RiccardoPerelli@users.noreply.github.com>
2023-03-08 12:33:44 +0100

    Initialize Time_fix

---
+++ Pandemood Project/Assets/Scripts/EnigmaGioia/PedanaPress.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PedanaPress : MonoBehaviour
+{
+    public float heightPress=0.05f;
+    public AudioSource AudioPedana;
+
+    private bool press=false;
+
+    void OnTriggerEnter()
+    {
+        if (!press)
+        {
+            var transform1 = transform;
+            var position = transform1.position;
+            position = new Vector3(position.x, position.y - heightPress, position.z);
+            transform1.position = position;
+            if (AudioPedana != null)
+                AudioPedana.Play();
+            press = true;
+        }
+    }
+
+    void OnTriggerExit()
+    {
+        if (press)
+        {
+            var transform1 = transform;
+            var position = transform1.position;
+            position = new Vector3(position.x, position.y + heightPress, position.z);
+            transform1.position = position;
+            if (AudioPedana != null)
+                AudioPedana.Play();
+            press = false;
+        }
+    }
+}
