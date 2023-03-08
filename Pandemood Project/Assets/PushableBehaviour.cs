5f674e40a949a2cd1f10555e1a52149970e826b6
RiccardoPerelli <39298954+RiccardoPerelli@users.noreply.github.com>
2023-03-08 12:31:55 +0100

    Initialize ric/charControl

---
+++ Pandemood Project/Assets/PushableBehaviour.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushableBehaviour : MonoBehaviour
+{
+
+    public GameObject player;
+    public Animator playerAnimator;
+    public float distanceFromPlayerThreshold = 1.8f;
+    public float newCapsuleColliderRadius = 0.48f;
+    private float _playerDistance;
+    private float _startingColliderDim;
+
+    private void Start()
+    {
+        _startingColliderDim = player.GetComponent<CapsuleCollider>().radius;
+    }
+
+    private void FixedUpdate()
+    {
+        if(Vector3.Distance(player.transform.position, transform.position) <= distanceFromPlayerThreshold)
+        {
+            player.GetComponent<CapsuleCollider>().radius = newCapsuleColliderRadius;
+        } 
+        else
+        {
+            player.GetComponent<CapsuleCollider>().radius = _startingColliderDim;
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.rigidbody != null)
+        {
+            if (collision.rigidbody.CompareTag("Player"))
+            {
+                if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) >= 0.95f)
+                {
+                    GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+                    if (playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("Pushing"))
+                    {
+                        GetComponent<Rigidbody>().isKinematic = false;
+                    } else
+                    {
+                        GetComponent<Rigidbody>().isKinematic = true;
+                    }
+                }
+                else
+                {
+                    GetComponent<Rigidbody>().isKinematic = false;
+                    GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezePositionZ;
+                }
+            }
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.rigidbody != null)
+        {
+            if (collision.rigidbody.CompareTag("Player"))
+            {
+                GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezePositionZ;
+            }
+        }
+    }
+}
---
+++ Pandemood Project/Assets/SpeedUpParticle.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedUpParticle : MonoBehaviour
+{
+
+    public ParticleSystem elevatorPSys;
+    public float SimulationSpeedMultiplier = 10f;
+    private bool _PlayerIsHere = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_PlayerIsHere)
+        {
+            float _vValue = Input.GetAxisRaw("Vertical");
+            if (_vValue >= 0.1f) {
+                var main = elevatorPSys.main;
+                main.simulationSpeed = _vValue * SimulationSpeedMultiplier;
+            }
+            else
+            {
+                var main = elevatorPSys.main;
+                main.simulationSpeed = 1f;
+            }
+        }
+        else
+        {
+            var main = elevatorPSys.main;
+            main.simulationSpeed = 1f;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player")) {
+            _PlayerIsHere = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            _PlayerIsHere = false;
+        }
+    }
+}
